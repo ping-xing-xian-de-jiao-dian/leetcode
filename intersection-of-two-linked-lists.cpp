96bf4bb8323ac7f507afd6fb31aeed39139ecf04
NAVER <1486466030@qq.com>
2021-08-30 14:11:25 +0800

    two_pointers_polish

--- intersection-of-two-linked-lists.cpp
+++ intersection-of-two-linked-lists.cpp
@@ -13,3 +13,16 @@ public:
         return NULL;
     }
 };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) return NULL;
+        ListNode* pa = headA, * pb = headB;
+        while (pa != pb){
+            if (!pa) pa = headB; else pa = pa->next;
+            if (!pb) pb = headA; else pb = pb->next;
+        }
+        return pa;
+    }
+};
