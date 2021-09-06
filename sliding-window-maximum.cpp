a86f740ae0edd0a1529a41a2e2679da3b522158c
NAVER <1486466030@qq.com>
2021-09-06 15:22:25 +0800

    monotone queue <--> deque

--- sliding-window-maximum.cpp
+++ sliding-window-maximum.cpp
@@ -1,3 +1,4 @@
+// priority_queue
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -19,6 +20,34 @@ public:
             } else {
                 que.pop();
             }
+        }
+        return res;
+    }
+};
+
+
+// monotone queue <--> deque
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // 看到这种问题可以想到单调栈，但是这里又需要对最前端的数据操作，因此需要队列
+        // 由此引出了“单调队列”！(吊打单调栈？)
+        // deque表示双端队列
+        size_t len = nums.size();
+        deque<int> que;
+        vector<int> res;
+        // 初始化（单调递减，因为位于左边且值还比新来的小的已经没有利用价值了）
+        // 但是只能存下标，因为存值的话就不知道遍历到哪里了
+        for (int i = 0; i < len; ++i){
+            while (!que.empty() && nums[i] > nums[que.back()]){
+                que.pop_back();
+            }
+            que.push_back(i);
+            while (que.front() <= i - k){
+                que.pop_front();
+            }
+            if (i < k - 1) continue;
+            res.emplace_back(nums[que.front()]);
         }
         return res;
     }
