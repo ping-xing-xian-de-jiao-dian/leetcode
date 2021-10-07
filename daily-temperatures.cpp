0409e6c38df5d02bdeb2dc526e5d6b6de60114a8
ping-xing-xian-de-jiao-dian <1486466030@qq.com>
2021-10-07 15:59:02 +0800

    easier

--- daily-temperatures.cpp
+++ daily-temperatures.cpp
@@ -7,16 +7,12 @@ public:
 
         for (int i = 0; i < len; ++i){
             auto t = temperatures[i];
-            if (s.empty() || t <= temperatures[s.top()]){
-                s.push(i);
-            } else {
-                while (!s.empty() && t > temperatures[s.top()]){
-                    int pos = s.top();
-                    res[pos] = i - pos;
-                    s.pop();
-                }
-                s.push(i);
+            while (!s.empty() && t > temperatures[s.top()]){
+                int pos = s.top();
+                res[pos] = i - pos;
+                s.pop();
             }
+            s.push(i);
         }
         return res;
     }
